0800b402554b651e75828821645f7e832ca88eff
kanhaiya <officialkanhaiya121@gmail.com>
2024-02-06 21:26:34 +0530

    first commit

---
+++ DuniyaJob/src/main/java/com/microden/in/DuniyaJobApplication.java
@@ -0,0 +1,13 @@
+package com.microden.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuniyaJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuniyaJobApplication.class, args);
+	}
+
+}
