b789b7f69b5a85d02ca402a27602b6963849119e
revathik123 <155417061+revathik123@users.noreply.github.com>
2024-02-08 13:30:37 +0530

    otp

--- DuniyaJob/src/main/java/com/microden/in/UserRepository.java
+++ DuniyaJob/src/main/java/com/microden/in/UserRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.microden.in.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long>{
-	
+	UserEntity findByemailAdress(String emailAdress);
+ 
 }
--- DuniyaJob/src/main/java/com/microden/in/controller/UserController.java
+++ DuniyaJob/src/main/java/com/microden/in/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microden.in.dto.Registration;
@@ -25,5 +26,12 @@ private UserService userservice;
 public UserEntity registeruser(@RequestBody Registration user){
 	
 	return userservice.registeruser(user);
+}
+//working on opt
+@PostMapping("/get-otp")
+public Boolean getOtp(@RequestParam String emailAdress, String otp ) {
+    
+    return userservice.getOtp(emailAdress, otp);
+	
 }
 }
